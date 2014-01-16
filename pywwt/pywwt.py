a56b1b1b02065104efec9d3272501f3444d0e273
John ZuHone <jzuhone@gmail.com>
2014-01-16 00:52:41 -0500

    Some refactoring. Didn't realize update can take more than one line.

--- pywwt/pywwt.py
+++ pywwt/pywwt.py
@@ -1,11 +1,9 @@
 import requests
+from requests import ConnectionError
 from bs4 import BeautifulSoup
 import logging
 requests_log = logging.getLogger("requests")
 requests_log.setLevel(logging.WARNING)
-from astropy.utils.console import ProgressBar
-from tempfile import mkstemp
-import csv
 
 def _handle_response(resp_str):
     soup = BeautifulSoup(resp_str)
@@ -45,6 +43,27 @@ class WWTLayer(object):
         props_str = u.text
         _handle_response(props_str)
 
+    def get_property(self, property_name):
+        params = {}
+        params["cmd"] = "getprop"
+        params["id"] = self.id
+        params["propname"] = property_name
+        u = requests.post(self.wwt.wwt_url, params=params)
+        prop_str = u.text
+        _handle_response(prop_str)
+        soup = BeautifulSoup(prop_str)
+        return soup.layer.attrs[property_name.lower()]
+
+    def get_properties(self):
+        params = {}
+        params["cmd"] = "getprops"
+        params["id"] = self.id
+        u = requests.post(self.wwt.wwt_url, params=params)
+        props_str = u.text
+        _handle_response(props_str)
+        soup = BeautifulSoup(props_str)
+        return soup.layer.attrs
+
     def update(self, data, no_purge=False, purge_all=False):
         params = {}
         params["cmd"] = "update"
@@ -52,12 +71,13 @@ class WWTLayer(object):
         params["purgeall"] = str(purge_all).lower()
         params["nopurge"] = str(no_purge).lower()
         fields = self.fields
+        data_string = ""
         nevents = len(data[fields[0]])
-        for i in ProgressBar(xrange(nevents)):
-            data_string = "\t".join([str(data[k][i]) for k in fields])
-            u = requests.post(self.wwt.wwt_url, params=params, data=data_string)
-            update_str = u.text
-            _handle_response(update_str)
+        for i in xrange(nevents):
+            data_string += "\t".join([str(data[k][i]) for k in fields])+"\n"
+        u = requests.post(self.wwt.wwt_url, params=params, data=data_string)
+        update_str = u.text
+        _handle_response(update_str)
 
     def activate(self):
         params = {}
@@ -75,6 +95,9 @@ class WWTLayer(object):
         layer_str = u.text
         _handle_response(layer_str)
 
+    def __str__(self):
+        return self.name
+
 class WWTController(object):
     def __init__(self, host=None):
         if host is None:
@@ -82,23 +105,21 @@ class WWTController(object):
         else:
             self.host = host
         self.wwt_url = "http://%s:5050/layerApi.aspx" % (self.host)
-        if not self._check_for_server():
-            raise WWTException("WWT has not been started at this address, " +
-                               "or is unreachable, or is not the required version.")
+        self._check_for_server()
 
     def _check_for_server(self):
+        params = {"cmd":"version"}
         try:
-            params = {"cmd":"version"}
             u = requests.get(self.wwt_url, params=params)
-            version_str = u.text
-            soup = BeautifulSoup(version_str)
-            tag = soup.layerapi.version
-            version_numbers = tag.string.split(".")
-            if float(".".join(version_numbers[:2])) > 2.8:
-                return True
-        except:
-            pass
-        return False
+        except ConnectionError:
+            raise WWTException("World Wide Telescope has not been started " +
+                               "on this host or is otherwise unreachable.")
+        version_str = u.text
+        soup = BeautifulSoup(version_str)
+        tag = soup.layerapi.version
+        version_numbers = tag.string.split(".")
+        if float(".".join(version_numbers[:2])) < 2.8:
+            raise WWTException("World Wide Telescope is not the required version (>= 2.8)")
 
     def change_mode(self, mode):
         params = {}
@@ -141,22 +162,11 @@ class WWTController(object):
     def load(self, source, frame, name, color=None,
              start_date=None, end_date=None,
              fade_type=None, fade_range=None):
-        if isinstance(source, basestring):
-            filename = source
-            f = open(filename, "r")
-            line = f.readline()
-            f.close()
-            fields = line.strip("\n").split(",")
-        elif isinstance(source, dict):
-            fields = source.keys()
-            handle, filename = mkstemp(suffix=".csv")
-            w = csv.DictWriter(handle, source.keys())
-            w.writeheader()
-            num_points = len(source.values()[0])
-            for i in ProgressBar(xrange(num_points)):
-                row = dict([(k,v[i]) for k,v in field_dict.items()])
-                w.writerow(row)
-            handle.close()
+        filename = source
+        f = open(filename, "r")
+        line = f.readline()
+        f.close()
+        fields = line.strip("\n").split(",")
         params = {}
         params["cmd"] = "load"
         params["filename"] = filename
@@ -181,20 +191,20 @@ class WWTController(object):
                   "name":name}
         u = requests.get(self.wwt_url, params=params)
         group_str = u.text
-        self._handle_response(group_str)
+        _handle_response(group_str)
 
     def ui_settings(self, setting_name, setting_val):
         params = {"cmd":"uisettings",
                   setting_name:setting_val}
         u = requests.get(self.wwt_url, params=params)
         ui_str = u.text
-        self._handle_response(ui_str)
+        _handle_response(ui_str)
 
     def get_state(self):
         params = {"cmd":"state"}
         u = requests.get(self.wwt_url, params=params)
         state_str = u.text
-        self._handle_response(state_str)
+        _handle_response(state_str)
         soup = BeautifulSoup(state_str)
         state = soup.layerapi.viewstate
         return state.attrs
@@ -204,10 +214,12 @@ class WWTController(object):
                   "layersonly":str(layers_only)}
         u = requests.get(self.wwt_url, params=params)
         layer_str = u.text
-        self._handle_response(layer_str)
+        _handle_response(layer_str)
         soup = BeautifulSoup(layer_str)
         layer_list = soup.layerapi.layerlist
         return layer_list
 
+    def __str__(self):
+        return "WWT client at %s" % (self.host)
 
     
