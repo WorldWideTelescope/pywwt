04c255d1aef48c1411a021ba3d9286a74bac4224
John ZuHone <jzuhone@gmail.com>
2014-01-19 23:27:09 -0500

    Docstrings, changing behavior of new and update, etc.
    Docstrings (still need to add examples). Added __repr__ for classes.
    
    When creating a new layer, no longer submit field names. Only do this
    on update.
    
    generate_utc_times now takes a time string as input for start_time as
    opposed to a datetime object.

--- pywwt/pywwt.py
+++ pywwt/pywwt.py
@@ -27,13 +27,26 @@ class WWTException(Exception):
     pass
 
 class WWTLayer(object):
-    def __init__(self, name, id, fields, wwt):
+    def __init__(self, name, id, wwt):
         self.name = name
         self.id = id
-        self.fields = fields
         self.wwt = wwt
 
     def set_property(self, property_name, property_value, **kwargs):
+        r"""
+        Set a single property. For a list of properties see:
+
+        http://www.worldwidetelescope.org/Developers/?LayerControlAPI#Table_of_Properties
+
+        Parameters
+        ----------
+        property_name : string
+            Name of the property to be set.
+        property_value : string
+            Value the property should take.
+
+        Also takes the standard keyword arguments.
+        """
         params = {}
         params["cmd"] = "setprop"
         params["id"] = self.id
@@ -45,6 +58,19 @@ class WWTLayer(object):
         _handle_response(prop_str)
 
     def set_properties(self, props_dict, **kwargs):
+        r"""
+        Set the properties of the layer. For a list of properties
+        see:
+
+        http://www.worldwidetelescope.org/Developers/?LayerControlAPI#Table_of_Properties
+
+        Parameters
+        ----------
+        props_dict : dict
+            Dictionary of {property_name, property_value} pairs.
+
+        Also takes the standard keyword arguments.
+        """
         props_string = "<?xml version='1.0' encoding='UTF-8'?><LayerApi><Layer "
         for key, value in props_dict.items():
             props_string += "%s=\"%s\" " % (key, value)
@@ -58,6 +84,20 @@ class WWTLayer(object):
         _handle_response(props_str)
 
     def get_property(self, property_name):
+        r"""
+        Return a property. For a list of properties see:
+
+        http://www.worldwidetelescope.org/Developers/?LayerControlAPI#Table_of_Properties
+
+        Parameters
+        ----------
+        property_name : string
+            The name of the property to be returned.
+
+        Returns
+        -------
+        The value of the property specified by *property_name*.
+        """
         params = {}
         params["cmd"] = "getprop"
         params["id"] = self.id
@@ -69,6 +109,16 @@ class WWTLayer(object):
         return soup.layer.attrs[property_name.lower()]
 
     def get_properties(self):
+        r"""
+        Return all the properties of the layer. For a list of properties
+        see:
+
+        http://www.worldwidetelescope.org/Developers/?LayerControlAPI#Table_of_Properties
+
+        Returns
+        -------
+        All of the properties in a dict of key, value pairs.
+        """
         params = {}
         params["cmd"] = "getprops"
         params["id"] = self.id
@@ -78,23 +128,59 @@ class WWTLayer(object):
         soup = BeautifulSoup(props_str)
         return soup.layer.attrs
 
-    def update(self, data, no_purge=False, purge_all=False, **kwargs):
+    def update(self, data=None, name=None,
+               no_purge=False, purge_all=False,
+               show=True, **kwargs):
+        r"""
+        Update the layer.
+
+        Parameters
+        ----------
+        data : dict of NumPy arrays, optional
+            New data to be added to the layer. Default is None.
+        name : string, optional
+            Rename the layer. Default is None.
+        no_purge : boolean, optional
+            The sending of an update command will delete events
+            that occur before the start time of any events in the
+            update and that have already decayed. Set this flag
+            if the event data should not be deleted. Defaults to
+            False.
+        purge_all : boolean, optional
+            Delete all data. Default is False.
+        show : boolean, optional
+            Whether to show or hide the layer. Default is True.
+
+        Also takes the standard keyword arguments.
+        """
         params = {}
         params["cmd"] = "update"
         params["id"] = self.id
+        if data is None:
+            params["hasheader"] = "False"
+        else:
+            params["hasheader"] = "True"
+        params["name"] = name
         params["purgeall"] = str(purge_all).lower()
         params["nopurge"] = str(no_purge).lower()
+        params["show"] = str(show).lower()
         _parse_kwargs(params, kwargs)
-        fields = self.fields
-        data_string = ""
-        nevents = len(data[fields[0]])
-        for i in xrange(nevents):
-            data_string += "\t".join([str(data[k][i]) for k in fields])+"\n"
+        if data is not None:
+            fields = data.keys()
+            data_string = "\t".join(fields)+"\n"
+            nevents = len(data[fields[0]])
+            for i in xrange(nevents):
+                data_string += "\t".join([str(data[k][i]) for k in fields])+"\n"
         u = requests.post(self.wwt.wwt_url, params=params, data=data_string)
         update_str = u.text
         _handle_response(update_str)
 
     def activate(self, **kwargs):
+        r"""
+        Highlight this layer in the layer manager.
+
+        Also takes the standard keyword arguments.
+        """
         params = {}
         params["cmd"] = "activate"
         params["id"] = self.id
@@ -104,6 +190,9 @@ class WWTLayer(object):
         _handle_response(layer_str)
 
     def delete(self):
+        r"""
+        Delete the layer.
+        """
         params = {}
         params["cmd"] = "delete"
         params["id"] = self.id
@@ -111,10 +200,23 @@ class WWTLayer(object):
         layer_str = u.text
         _handle_response(layer_str)
 
+    def __repr__(self):
+        return "<WWTLayer \"%s\" with ID %s on client at %s>" % (
+            self.name, self.id, self.wwt.host)
+
     def __str__(self):
         return self.name
 
 class WWTController(object):
+    r"""
+    Initialize a WWTController, connecting to a WWT client.
+
+    Parameters
+    ----------
+    host : string
+        String representing the hostname or IP address where
+        the WWT client is being run. Defaults to "127.0.0.1".
+    """
     def __init__(self, host=None):
         if host is None:
             self.host = "127.0.0.1"
@@ -140,6 +242,16 @@ class WWTController(object):
             raise WWTException("World Wide Telescope is not the required version (>= 2.8)")
 
     def change_mode(self, mode, **kwargs):
+        r"""
+        Changes the view to one of Earth, Planet, Sky, Panorama, SolarSystem.
+
+        Parameters
+        ----------
+        mode : string
+            The mode to change to.
+
+        Also accepts the standard keyword arguments.
+        """
         params = {}
         params["cmd"] = "mode"
         params["lookat"] = mode
@@ -149,20 +261,69 @@ class WWTController(object):
         _handle_response(mode_str)
 
     def move_view(self, parameter, **kwargs):
+        r"""
+        Changes the view depending on the supplied parameter.
+        For a list of parameters see:
+
+        http://www.worldwidetelescope.org/Developers/?LayerControlAPI#move
+
+        Parameters
+        ----------
+        parameter : string
+            The parameter used to set the view.
+
+        Also accepts the standard keyword arguments.
+        """
         params = {"cmd":"move", "move":parameter}
         _parse_kwargs(params, kwargs)
         u = requests.get(self.wwt_url, params=params)
         move_str = u.text
         _handle_response(move_str)
 
-    def new_layer(self, frame, name, fields=None,
+    def new_layer(self, frame, name,
                   color=None, start_date=None,
                   end_date=None, fade_type=None,
                   fade_range=None, **kwargs):
-        if fields is None:
-            field_string = ""
-        else:
-            field_string = "\t".join(fields)
+        r"""
+        Initialize a new layer in the layer manager.
+
+        Parameters
+        ----------
+        frame : string
+            The reference frame of the layer.
+        name : string
+            The name of the layer.
+        color : hex string, optional
+            ARBG hex value of the color to be used when rendering
+            the events of the layer. Defaults to "FFFFFFFF" (white).
+        start_date : string, optional
+            With time series data, the date and time to start the
+            visualization for this layer. This could for example
+            be slightly earlier than the date of the first event
+            in the actual data. Defaults to the system minimum
+            date value.
+            Formats (month/day/year):
+            "1/1/2010 11:00:00 PM"
+            "1/1/2010 11:30 AM"
+            "1/1/2010 11 am"
+            "1/1/2000"
+            "1/2000"
+        end_date : string, optional
+            With time series data, the date and time to end the
+            visualization for this layer. Defaults to the system
+            maximum date value.
+        fade_type : string, optional
+            Fades the data visualization. One of: "In", "Out", "Both",
+            or "None". Defaults to "None".
+        fade_range : int, optional
+            Fade time in days. Defaults to 0.
+
+        Also accepts the default keyword arguments.
+
+        Returns
+        -------
+        A `WWTLayer` object corresponding to the layer just created.
+        """
         params = {}
         params["cmd"] = "new"
         params["name"] = name
@@ -172,22 +333,63 @@ class WWTController(object):
         params["fadetype"] = fade_type
         params["faderange"] = fade_range
         _parse_kwargs(params, kwargs)
-        u = requests.post(self.wwt_url, params=params, data=field_string)
+        u = requests.post(self.wwt_url, params=params)
         layer_str = u.text
         soup = BeautifulSoup(layer_str)
         layer_id = soup.layerapi.findChild(name="newlayerid").string
         if len(layer_id) != 36:
             raise WWTException("Invalid Layer ID received")
-        return WWTLayer(name, layer_id, fields, self)
+        return WWTLayer(name, layer_id, self)
 
-    def load(self, source, frame, name, color=None,
+    def load(self, filename, frame, name, color=None,
              start_date=None, end_date=None,
              fade_type=None, fade_range=None, **kwargs):
-        filename = source
+        r"""
+        Initialize a new layer in the layer manager and load data
+        from a file into it.
+
+        Parameters
+        ----------
+        filename : string
+            The full path of the file to load.
+        frame : string
+            The reference frame of the layer.
+        name : string
+            The name of the layer.
+        color : hex string, optional
+            ARBG hex value of the color to be used when rendering
+            the events of the layer. Defaults to "FFFFFFFF" (white).
+        start_date : string, optional
+            With time series data, the date and time to start the
+            visualization for this layer. This could for example
+            be slightly earlier than the date of the first event
+            in the actual data. Defaults to the system minimum
+            date value.
+            Formats (month/day/year):
+            "1/1/2010 11:00:00 PM"
+            "1/1/2010 11:30 AM"
+            "1/1/2010 11 am"
+            "1/1/2000"
+            "1/2000"
+        end_date : string, optional
+            With time series data, the date and time to end the
+            visualization for this layer. Defaults to the system
+            maximum date value.
+        fade_type : string, optional
+            Fades the data visualization. One of: "In", "Out", "Both",
+            or "None". Defaults to "None".
+        fade_range : int, optional
+            Fade time in days. Defaults to 0.
+
+        Also accepts the default keyword arguments.
+
+        Returns
+        -------
+        A `WWTLayer` object corresponding to the layer just created.
+        """
         f = open(filename, "r")
         line = f.readline()
         f.close()
-        fields = line.strip("\n").split(",")
         params = {}
         params["cmd"] = "load"
         params["filename"] = filename
@@ -205,9 +407,21 @@ class WWTController(object):
         layer_id = soup.layerapi.findChild(name="newlayerid").string
         if len(layer_id) != 36:
             raise WWTException("Invalid Layer ID received")
-        return WWTLayer(name, layer_id, fields, self)
+        return WWTLayer(name, layer_id, self)
 
     def new_layer_group(self, frame, name, **kwargs):
+        r"""
+        Specifies that a layer group should be added.
+
+        Parameters
+        ----------
+        frame : string
+            The reference frame of the layer group.
+        name : string
+            The name of the layer group.
+
+        Also accepts the standard keyword arguments.
+        """
         params = {"cmd":"group",
                   "frame":frame,
                   "name":name}
@@ -217,6 +431,21 @@ class WWTController(object):
         _handle_response(group_str)
 
     def ui_settings(self, setting_name, setting_val, **kwargs):
+        r"""
+        Change user interface settings, without altering the
+        layer data. For the settings list see:
+
+        http://www.worldwidetelescope.org/Developers/?LayerControlAPI#uisettings
+
+        Parameters
+        ----------
+        setting_name : string
+            The name of the setting to be changed.
+        setting_val : string
+            The value to set the setting to.
+
+        Also takes the standard keyword arguments.
+        """
         params = {"cmd":"uisettings",
                   setting_name:setting_val}
         _parse_kwargs(params, kwargs)
@@ -225,6 +454,13 @@ class WWTController(object):
         _handle_response(ui_str)
 
     def get_state(self):
+        r"""
+        Requests some details of the current view.
+
+        Returns
+        -------
+        The state information as a dict of key, value pairs.
+        """
         params = {"cmd":"state"}
         u = requests.get(self.wwt_url, params=params)
         state_str = u.text
@@ -234,6 +470,20 @@ class WWTController(object):
         return state.attrs
 
     def get_layer_list(self, layers_only=True):
+        r"""
+        Returns the structure of the layers and layer group names
+        that are currently in the layer manager.
+
+        Parameters
+        ----------
+        layers_only : boolean, optional
+            Indicates that only layers, and not reference frames
+            or group names, should be returned.
+
+        Returns
+        -------
+        A BeautifulSoup representation of the XML-formatted layer list.
+        """
         params = {"cmd":"layerlist",
                   "layersonly":str(layers_only)}
         u = requests.get(self.wwt_url, params=params)
@@ -243,7 +493,7 @@ class WWTController(object):
         layer_list = soup.layerapi.layerlist
         return layer_list
 
-    def __str__(self):
-        return "WWT client at %s" % (self.host)
+    def __repr__(self):
+        return "WWTController(host=\"%s\")" % (self.host)
 
     
--- pywwt/utils.py
+++ pywwt/utils.py
@@ -5,11 +5,33 @@ from matplotlib.colors import LogNorm, Normalize
 import struct
 from astropy.utils.console import ProgressBar
 from datetime import datetime, timedelta
-from dateutil import tz
+from dateutil import tz, parser
 
 def map_array_to_colors(arr, cmap, scale="linear",
                         vmin=None, vmax=None):
+    r"""
+    Map a NumPy array to a colormap using Matplotlib.
 
+    Parameters
+    ----------
+    arr : NumPy array of floats or ints
+        The array to be mapped onto a colormap.
+    cmap : string
+        The name of a Matplotlib colormap.
+    scale : string, optional
+        The scaling of the data as mapped to the
+        colormap. Takes "linear" or "log", default "linear".
+    vmin : float or int
+        The value associated with the lowest end of the colormap.
+        Defaults to the minimum value of *arr*.
+    vmax : float or int
+        The value associated with the highest end of the colormap.
+        Defaults to the maximum value of *arr*.
+
+    Returns
+    -------
+    A list of colors, encoded in ARGB hex values.
+    """
     if vmin is None:
         vmin = arr.min()
     if vmax is None:
@@ -29,10 +51,37 @@ def map_array_to_colors(arr, cmap, scale="linear",
     return colors
 
 def generate_utc_times(num_steps, step_size, start_time=None):
+    r"""
+    Generate a series of equally linearly spaced times in UTC.
 
+    Parameters
+    ----------
+    num_steps : int
+        The number of times to generate.
+    step_size : dict
+        A dictionary corresponding to the step size between
+        the times, with keys referring to the unit (seconds,
+        minutes, days, etc.) and the values of the step in that unit.
+    start_time : string, optional
+        A string corresponding to a time in the local time zone
+        at which to start the timesteps. Defaults to the current
+        system time.
+        Formats (month/day/year):
+        "1/1/2010 11:00:00 PM"
+        "1/1/2010 11:30 AM"
+        "1/1/2010 11 am"
+        "1/1/2000"
+        "1/2000"
+
+    Returns
+    -------
+    A list of formatted date/time strings in UTC readable by
+    World Wide Telescope.
+    """
     if start_time is None:
         start_time = datetime.utcnow()
     else:
+        start_time = parser.parse(start_time)
         utc_zone = tz.tzutc()
         local_zone = tz.tzlocal()
         # Tell the datetime object that it's in local time zone since
@@ -50,6 +99,30 @@ def generate_utc_times(num_steps, step_size, start_time=None):
     return time_arr
 
 def convert_xyz_to_spherical(x, y, z, is_astro=True, ra_units="degrees"):
+    r"""
+    Convert rectangular coordinates (x,y,z) to spherical coordinates
+    (Lat, Lon, Alt) or (RA, Dec, Alt).
+
+    Parameters
+    ----------
+    x : NumPy array of floats
+        The x-coordinates of the data.
+    y : NumPy array of floats
+        The y-coordinates of the data.
+    z : NumPy array of floats
+        The z-coordinates of the data.
+    is_astro : boolean, optional
+        Is the coordinate system astronomical (RA, Dec)
+        or geographical (Lat, Lon)? Defaults to True.
+    ra_units : string, optional
+        The unit of the RA/Lon coordinate, "degrees" or
+        "hours". Defaults to "degrees".
+
+    Returns
+    -------
+    A dict of NumPy arrays corresponding to the positions in
+    spherical coordinates.
+    """
     if ra_units == "degrees" or not is_astro:
         ra_scale = 1.
     elif ra_units == "hours":
@@ -66,10 +139,27 @@ def convert_xyz_to_spherical(x, y, z, is_astro=True, ra_units="degrees"):
     coords[dec_name] = np.rad2deg(np.arccos(z/coords["ALT"])) - 90.
     return coords
 
-def write_data_to_csv(data, filename, mode="wb"):
-    f = open(filename, mode)
+def write_data_to_csv(data, filename, mode="new"):
+    r"""
+    Write a dataset to a CSV-formatted file with a data header.
+
+    Parameters
+    ----------
+    data : dictionary of NumPy arrays
+        The data to be written.
+    filename : string
+        The filename to write the data to.
+    mode : string, optional
+        Write a "new" file or "append" to an existing file?
+        Default "new".
+    """
+    if mode == "new":
+        fmode = "wb"
+    elif mode == "append":
+        fmode = "a+b"
+    f = open(filename, fmode)
     w = csv.DictWriter(f, data.keys())
-    if mode == "wb": w.writeheader()
+    if mode == "new": w.writeheader()
     num_points = len(data.values()[0])
     for i in ProgressBar(xrange(num_points)):
         row = dict([(k,v[i]) for k,v in data.items()])
