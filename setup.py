e4b133e776faa6da00f2b15629c40b9e3de48e5b
John ZuHone <jzuhone@gmail.com>
2014-01-19 23:46:20 -0500

    Including setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='pywwt',
+      version='0.1.0',
+      description = 'Python interface to World Wide Telescope',
+      author='John ZuHone',
+      author_email='jzuhone@gmail.com',
+      url='http://github.com/jzuhone/pywwt',
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering :: Data Visualization',
+          ],
+      packages = find_packages()
+    )
+
