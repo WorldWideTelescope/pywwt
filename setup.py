f9e84e30c8b106bfbf4fec4f6e0b3d75ed8dc6f2
John ZuHone <jzuhone@gmail.com>
2014-01-22 22:09:16 -0500

    Small distutils fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='pywwt',
           'Intended Audience :: Science/Research',
           'Operating System :: OS Independent',
           'Programming Language :: Python :: 2.7',
-          'Topic :: Scientific/Engineering :: Data Visualization',
+          'Topic :: Scientific/Engineering :: Visualization',
           ],
       packages = find_packages()
     )
