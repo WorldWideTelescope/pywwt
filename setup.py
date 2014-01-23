e904e19e6da816008eb8624e0f7916463be57695
John ZuHone <jzuhone@gmail.com>
2014-01-22 22:13:07 -0500

    handling version tags

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pywwt',
-      version='0.1.0',
+      version='0.1.1-dev',
       description = 'Python interface to World Wide Telescope',
       author='John ZuHone',
       author_email='jzuhone@gmail.com',
