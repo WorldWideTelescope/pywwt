217300f6f9d1935d4ce159000423bcd42f29d090
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-12-04 15:09:18 +0000

    Fix API docs output

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -180,4 +180,5 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-automodsumm_inherited_members = False
+numpydoc_show_class_members = False
+
