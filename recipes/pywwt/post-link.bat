71a7e6c5a500c38700792873e6a5467e92332ff0
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-04-05 22:24:36 +0100

    Include jupyterlab in dependencies and enable lab and server extensions

--- recipes/pywwt/post-link.bat
+++ recipes/pywwt/post-link.bat
@@ -3,3 +3,7 @@
 "%PREFIX%\Scripts\jupyter-nbextension.exe" enable widgetsnbextension --py --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
 "%PREFIX%\Scripts\jupyter-nbextension.exe" enable pywwt --py --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
 
+"%PREFIX%\Scripts\jupyter-labextension.exe" enable @jupyter-widgets/jupyterlab-manager --py --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-labextension.exe" enable pywwt --py --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
+
+"%PREFIX%\Scripts\jupyter-serverextension.exe" enable pywwt --py --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
--- recipes/pywwt/post-link.sh
+++ recipes/pywwt/post-link.sh
@@ -1,2 +1,8 @@
 "${PREFIX}/bin/jupyter-nbextension" enable widgetsnbextension --py --sys-prefix >> $PREFIX/.messages.txt 2>&1
 "${PREFIX}/bin/jupyter-nbextension" enable pywwt --py --sys-prefix >> $PREFIX/.messages.txt 2>&1
+
+"${PREFIX}/bin/jupyter-labextension" enable pywwt --py --sys-prefix >> $PREFIX/.messages.txt 2>&1
+"${PREFIX}/bin/jupyter-labextension" enable @jupyter-widgets/jupyterlab-manager --py --sys-prefix >> $PREFIX/.messages.txt 2>&1
+
+"${PREFIX}/bin/jupyter-serverextension" enable pywwt --py --sys-prefix >> $PREFIX/.messages.txt 2>&1
+
