d746d22562c79366ea9ddf3313c8ca3b95e1e1d7
O . O <ojustino@users.noreply.github.com>
2018-11-28 17:37:08 -0500

    Merged NIRCAM entries in instruments.json

--- gen_instruments.py
+++ gen_instruments.py
@@ -2,8 +2,6 @@ import json
 import requests
 from astropy import units as u
 
-to_json = {}
-
 def draw_rect(l, w, center):
     # get corners of a rectangle when given center plus its length and width
     ras = []; decs = []
@@ -27,7 +25,6 @@ def draw_rect(l, w, center):
 
 def draw_k2():
     # get file with k2 coordinates.
-    # NOTE THAT THESE VALUES ARE IN DEGREES WHILE OTHER INSTS ARE IN ARCSECONDS
     ras = []; decs = []
     all_coords = []
     
@@ -51,9 +48,9 @@ def draw_k2():
 
     return all_coords
 
-
-# dimensions here are in arcseconds for ease of entry (except for k2)
-# they are converted to degrees in a later loop
+# dimension values written here should be in arcseconds for ease of entry;
+# they are converted to degrees in a later loop.
+# (absolute FOVs, like k2, may come in  other units depending on their source)
 instruments = {'hst_acs_wfc':   {'pos': 'relative', 'l': 202, 'w': 100,
                                  'build': 'curl', 'panels': 2, 'gap': [2, 0]},
                # ^ total acs_wfc dimensions: 202 x 202 ^
@@ -64,7 +61,8 @@ instruments = {'hst_acs_wfc':   {'pos': 'relative', 'l': 202, 'w': 100,
                                  'build': 'curl', 'panels': 1, 'gap': None},
                'jwst_nircam':   {'pos': 'relative', 'l': 129, 'w': 129,
                                  'build': 'curl', 'panels': 1, 'gap': None},
-               'nircam_short':  {'pos': 'relative', 'l': 64, 'w': 64,
+               'jwst_nircam_short':
+                                {'pos': 'relative', 'l': 64, 'w': 64,
                                  'build': 'curl', 'panels': 4, 'gap': [4.5, 4]},
                'jwst_niriss':   {'pos': 'relative', 'l': 133, 'w': 133,
                                  'build': 'curl', 'panels': 1, 'gap': None},
@@ -73,7 +71,11 @@ instruments = {'hst_acs_wfc':   {'pos': 'relative', 'l': 202, 'w': 100,
                'k2':            {'pos': 'absolute'}
 }
 
-for inst, specs in instruments.items():
+# the dict to be filled with entries for each FOV
+to_json = {}
+
+# build each entry in to_json, instrument by instrument
+for inst, specs in sorted(instruments.items()):
     if inst == 'k2':
         all_coords = draw_k2()
         to_json[inst] = [specs['pos'], all_coords]
@@ -115,7 +117,6 @@ for inst, specs in instruments.items():
             
     i = 0
     while i < specs['panels']:
-
         if specs['build'] == 'curl':
             if i % 3 != 0:
                 cx = abs(center_x)
@@ -132,7 +133,6 @@ for inst, specs in instruments.items():
                     cy -= .5/3600
 
             coords = draw_rect(specs['l'], specs['w'], (cx, cy))
-            all_coords.append([coords[0], coords[1]])
             # each set of coords for a shape should be a list within all_coords.
             # for a two-panel rectangular FOV, all_coords should contain
             # two len 2 lists. each of those should contain two len 4 lists.
@@ -140,20 +140,22 @@ for inst, specs in instruments.items():
 
         elif specs['build'] == 'stack':
             coords = draw_rect(specs['l'], specs['w'], (center_x, center_y))
-            all_coords.append([coords[0], coords[1]])
             center_y += add
 
+        all_coords.append([coords[0], coords[1]])
         i += 1
 
-    to_json[inst] = [specs['pos'], all_coords]
+    # create the final entry to be converted to JSON for each FOV here.
+    # cases for panels that should be joined as one entry are if conditions
+    if inst == 'jwst_nircam_short':
+        to_json['jwst_nircam'][-1].extend(all_coords)
+    else:
+        to_json[inst] = [specs['pos'], all_coords]
 
-#from pprint import pprint
-#pprint(to_json['hst_wfc3_ir'])
-#pprint(to_json['spitzer_irac'])
-#pprint(to_json['jwst_nircam'])
-#pprint(to_json['nircam_short'])  # exemplars
-#pprint(to_json['k2'])
+# convert to JSON and write the contents to instruments.json
 with open('instruments.json', 'w') as output:
     json.dump(to_json, output, sort_keys=True)#, indent=2)
+
+#from pprint import pprint
 #full_json = json.dumps(to_json, sort_keys=True)#, indent=2)
 #pprint(full_json)
