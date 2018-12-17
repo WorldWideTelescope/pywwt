d67c89b8c8697f8ba34ee8b3e01aedf225b965ea
O . O <ojustino@users.noreply.github.com>
2018-12-17 14:38:15 -0500

    Switched URL in instruments.py

--- pywwt/instruments.py
+++ pywwt/instruments.py
@@ -5,11 +5,8 @@ class Instruments():
     A supplemental class that enables tab-completion for available
     instruments in FieldOfView().
     """
-     # change link once new gh-pages PR is accepted!
-    
     def __init__(self):
-        # instruments = requests.get('https://worldwidetelescope.github.io/pywwt/instruments.json').json()
-        self.entry = requests.get('https://raw.githubusercontent.com/WorldWideTelescope/pywwt/d746d22562c79366ea9ddf3313c8ca3b95e1e1d7/instruments.json').json()
+        self.entry = requests.get('https://worldwidetelescope.github.io/pywwt/instruments.json').json()
         self.available = list(self.entry.keys())
 
     def __dir__(self):
