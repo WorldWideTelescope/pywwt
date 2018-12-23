ce9f5c9d3e657038b5afcaacca4b49f547141f92
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-12-23 11:34:36 +0000

    Preparing release v0.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -62,7 +62,7 @@ copyright = '2017, ' + author
 # built documents.
 #
 # The short X.Y version.
-version = '0.4.0'
+version = '0.4.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
