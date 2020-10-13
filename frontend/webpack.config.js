932219cd249c3ed9e731170df652d138ea3ff207
Peter Williams <peter@newton.cx>
2020-10-13 10:52:27 -0400

    frontend: isolate the fronted JS package here

---
+++ frontend/webpack.config.js
@@ -0,0 +1,26 @@
+var loaders = [
+  { test: /\.ts$/, loader: 'ts-loader' },
+  { test: /\.js$/, loader: "source-map-loader" },
+];
+
+module.exports = [
+  {
+    entry: './lib/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+        library: "pywwt",
+        libraryTarget: 'amd'
+    },
+    module: {
+      loaders: loaders
+    },
+    devtool: 'source-map',
+    externals: ['@jupyter-widgets/base'],
+    resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+    },
+
+  },
+];
