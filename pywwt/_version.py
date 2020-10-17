a938e749302aae9e5d2dcbbc16ced8de4bd32583
Peter Williams <peter@newton.cx>
2020-10-17 12:39:52 -0400

    Prep for Cranko bootstrap

--- pywwt/_version.py
+++ pywwt/_version.py
@@ -1,4 +1,4 @@
-version_info = (0, 10, 0, 'dev', 0)
+version_info = (0, 9, 0, 'final', 0)  # cranko project-version tuple
 
 _specifier_ = {'alpha': '.a', 'beta': '.b', 'candidate': '.rc', 'final': '', 'dev': '.dev'}
 
