5c4d040bcbc6d9973a1457c149788e0f7704e477
Peter Williams <peter@newton.cx>
2020-10-17 13:07:23 -0400

    Switch to Cranko for versioning.

--- pywwt/_version.py
+++ pywwt/_version.py
@@ -1,4 +1,4 @@
-version_info = (0, 9, 0, 'final', 0)  # cranko project-version tuple
+version_info = (0, 0, 0, 'dev', 0)  # cranko project-version tuple
 
 _specifier_ = {'alpha': '.a', 'beta': '.b', 'candidate': '.rc', 'final': '', 'dev': '.dev'}
 
