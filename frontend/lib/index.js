54a5bd52e68d679b1eda562d53ceee67b88922f1
Peter Williams <peter@newton.cx>
2020-10-20 18:43:27 -0400

    frontend/lib/index.js: remove some outdated/unhelpful comments

--- frontend/lib/index.js
+++ frontend/lib/index.js
@@ -1,7 +1,2 @@
-// Entry point for the notebook bundle containing custom model definitions.
-//
-// Setup notebook base URL
-
-// Export widget models and views, and the npm package version number.
 module.exports = require('./wwt.js');
 module.exports['version'] = require('../package.json').version;
