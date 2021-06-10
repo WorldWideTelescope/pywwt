2744d891ffcf237ecfeb3753c47f4543baba20a1
Peter Williams <peter@newton.cx>
2021-06-10 11:28:32 -0400

    frontend: work on tidying up internal documentation

--- frontend/lib/index.js
+++ frontend/lib/index.js
@@ -1,2 +1,12 @@
+// Copyright 2021 the .NET Foundation
+// Licensed under the MIT License
+
+// This is the main index file for the pywwt Jupyter Widget frontend JavaScript
+// module. `widget.js` contains the main ipywidgets implementation, and
+// `jupyterlab.js` includes the JupyterLab extension shims needed to make the
+// widget available in the JupyterLab environment. `jupyterlab.js` isn't
+// imported here, but the `package.json` contains metadata telling the
+// JupyterLab extension manager to load up that file.
+
 module.exports = require('./widget.js');
 module.exports['version'] = require('../package.json').version;
--- frontend/lib/jupyterlab.js
+++ frontend/lib/jupyterlab.js
@@ -1,8 +1,7 @@
 // This file implements the JupyterLab extension, as linked up through this
 // module's `package.json` file (`jupyerlab.extension` keyword).
 
-var wwt = require('./index');
-
+var main_module = require('./index');
 var base = require('@jupyter-widgets/base');
 
 /**
@@ -11,12 +10,12 @@ var base = require('@jupyter-widgets/base');
 module.exports = {
   id: 'pywwt',
   requires: [base.IJupyterWidgetRegistry],
-  activate: function(app, widgets) {
-      widgets.registerWidget({
-          name: 'pywwt',
-          version: wwt.version,
-          exports: wwt
-      });
-    },
+  activate: function (app, widgets) {
+    widgets.registerWidget({
+      name: 'pywwt',
+      version: main_module.version,
+      exports: main_module
+    });
+  },
   autoStart: true
 };
