2a0a0800bfb476db55b6d1a4069cbf5af6d0fed8
Peter Williams <peter@newton.cx>
2021-12-13 13:33:57 -0500

    pywwt/utils.py: reformat with "black"

--- pywwt/utils.py
+++ pywwt/utils.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from reproject import reproject_interp
 from reproject.mosaicking import find_optimal_celestial_wcs
 
-__all__ = ['sanitize_image']
+__all__ = ["sanitize_image"]
 
 
 def sanitize_image(image, output_file, overwrite=False, hdu_index=None, **kwargs):
@@ -26,8 +26,11 @@ def sanitize_image(image, output_file, overwrite=False, hdu_index=None, **kwargs
                 image = hdul[hdu_index]
             else:
                 for hdu in hdul:
-                    if (hasattr(hdu, 'shape') and len(hdu.shape) > 1
-                            and type(hdu) is not fits.hdu.table.BinTableHDU):
+                    if (
+                        hasattr(hdu, "shape")
+                        and len(hdu.shape) > 1
+                        and type(hdu) is not fits.hdu.table.BinTableHDU
+                    ):
                         break
                 image = hdu
             transform_to_wwt_supported_fits(image, output_file, overwrite)
@@ -36,32 +39,35 @@ def sanitize_image(image, output_file, overwrite=False, hdu_index=None, **kwargs
 
 
 def transform_to_wwt_supported_fits(image, output_file, overwrite):
-    wcs, shape_out = find_optimal_celestial_wcs([image], frame=ICRS(),
-                                                projection='TAN')
+    wcs, shape_out = find_optimal_celestial_wcs([image], frame=ICRS(), projection="TAN")
     array = reproject_interp(image, wcs, shape_out=shape_out, return_footprint=False)
-    fits.writeto(output_file, array.astype(np.float32),
-                 wcs.to_header(), overwrite=overwrite)
+    fits.writeto(
+        output_file, array.astype(np.float32), wcs.to_header(), overwrite=overwrite
+    )
 
 
 def validate_traits(cls, traits):
-    '''
+    """
     Helper function to ensure user-provided trait names match those of the
     class they're being used to instantiate.
-    '''
+    """
     mismatch = [key for key in traits if key not in cls.trait_names()]
     if mismatch:
-        raise KeyError('Key{0} {1} do{2}n\'t match any layer trait name'
-                       .format('s' if len(mismatch) > 1 else '',
-                               mismatch,
-                               '' if len(mismatch) > 1 else 'es'))
+        raise KeyError(
+            "Key{0} {1} do{2}n't match any layer trait name".format(
+                "s" if len(mismatch) > 1 else "",
+                mismatch,
+                "" if len(mismatch) > 1 else "es",
+            )
+        )
 
 
 def ensure_utc(tm, str_allowed):
-    '''
+    """
     Helper function to convert a time object (Time, datetime, or UTC string
     if str_allowed == True) into UTC before passing it to WWT.
     str_allowed is True for wwt.set_current_time (core.py) and False for TableLayer's 'time_att' implementation (layers.py).
-    '''
+    """
 
     if tm is None:
         utc_tm = datetime.utcnow().astimezone(pytz.UTC).isoformat()
@@ -79,9 +85,9 @@ def ensure_utc(tm, str_allowed):
 
     else:
         if str_allowed:  # is an ISOT string
-            dt = Time(tm, format='isot').to_datetime(pytz.UTC)
+            dt = Time(tm, format="isot").to_datetime(pytz.UTC)
             utc_tm = dt.isoformat()
         else:
-            raise ValueError('Time must be a datetime or astropy.Time object')
+            raise ValueError("Time must be a datetime or astropy.Time object")
 
     return utc_tm
