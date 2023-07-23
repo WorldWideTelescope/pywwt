977652a085c5264cf4ad69a7a8b1f98e93cd6ca6
Peter Williams <peter@newton.cx>
2023-07-23 16:39:15 -0400

    Fix compatibility with `notebook` 7.x

--- .check_enabled.py
+++ .check_enabled.py
@@ -2,22 +2,32 @@ import logging
 import sys
 
 from jupyterlab.commands import get_app_info
-from notebook.nbextensions import validate_nbextension
-from notebook.serverextensions import validate_serverextension
+
+try:
+    from nbclassic.nbextensions import validate_nbextension
+except ImportError:
+    # `notebook` <= 6
+    from notebook.nbextensions import validate_nbextension
+
+try:
+    from nbclassic.serverextensions import validate_serverextension
+except ImportError:
+    # `notebook` <= 6
+    from notebook.serverextensions import validate_serverextension
 
 # If there's a problem and we don't provide this, the validate function crashes :-(
-logger = logging.getLogger('')
+logger = logging.getLogger("")
 
-if validate_nbextension('pywwt/extension', logger=logger) != []:
+if validate_nbextension("pywwt/extension", logger=logger) != []:
     print("Issue detected with nbextension")
     sys.exit(1)
 
 info = get_app_info()
 
-if 'pywwt' not in info['extensions'] or 'pywwt' in info['disabled']:
+if "pywwt" not in info["extensions"] or "pywwt" in info["disabled"]:
     print("Issue detected with labextension")
     sys.exit(1)
 
-if validate_serverextension('pywwt', logger=logger) != []:
+if validate_serverextension("pywwt", logger=logger) != []:
     print("Issue detected with serverextension")
     sys.exit(1)
